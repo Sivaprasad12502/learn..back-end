cc2c79802eb3d6da5fc685b67dbcc698cf136b70
sivaprasad <sivaprasad122003.com>
2025-01-31 16:13:06 +0530

    fixed

--- script.js
+++ script.js
@@ -2,17 +2,15 @@
 // const textIn=fs.readFileSync('./txt/input.txt', 'utf-8')
 // console.log(textIn)
 
-
 //10 reding and wirting Files Asynchronously
 
-// Blocking , synchrounous way 
+// Blocking , synchrounous way
 // const fs=require('fs')
 // const textIn = fs.readFileSync('/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/txt/output.txt','utf-8')
 // console.log(textIn)
 // const textOut=`This is what we know: ${textIn}.\nCreated on ${Date.now()}`
 // fs.writeFileSync('/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/txt/output.txt',textOut)
 // console.log('File written!')
-
 
 // Non-blocking asynchronous way
 // const fs=require('fs')
@@ -34,73 +32,91 @@
 // console.log('will read file!')
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
-const fs=require('fs')
-const http=require('http')
-const url=require('url')
-const replaceTemplate= require('/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/modules/replaceTemplate.js')
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
+const slugify = require("slugify");
+
+const replaceTemplate = require("/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/modules/replaceTemplate.js");
 
 /////////////
 ///SERVER
 
+const tempOveriew = fs.readFileSync(
+  "/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/templates/template-overview.html",
+  "utf-8"
+);
+const tempcard = fs.readFileSync(
+  "/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/templates/template-card-html",
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  "/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/templates/template-product.html",
+  "utf-8"
+);
 
+const data = fs.readFileSync(
+  "/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/dev-data/data.json",
+  "utf-8"
+);
+const dataObj = JSON.parse(data);
+const emojiToId={
+    "🥑": 0,  // Fresh Avocados
+    "🧀": 1,  // Goat and Sheep Cheese
+    "🥦": 2,  // Apollo Broccoli
+    "🥕": 3,  // Baby Carrots
+    "🌽": 4   // Sweet Corncobs
+}
 
-const tempOveriew=fs.readFileSync('/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/templates/template-overview.html','utf-8',)
-const tempcard=fs.readFileSync('/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/templates/template-card-html','utf-8',)
-const tempProduct=fs.readFileSync('/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/templates/template-product.html','utf-8')
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
-const data=fs.readFileSync('/home/vonnue/Desktop/Siva/back-end/complete-node-bootcamp/1-node-farm/starter/dev-data/data.json','utf-8',)
-const dataObj=JSON.parse(data)
+console.log(slugify("Fresh Avocados", { lower: true }));
 
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
 
-const server=http.createServer((req,res)=>{
-    const {query, pathname}=url.parse(req.url,true)
+  //Overview Page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
 
-    //Overview Page
-    if(pathname === '/'|| pathname=== '/overview'){
-
-        res.writeHead(200,{'Content-type':'text/html'})
-
-       const cardsHtml= dataObj.map(el=>replaceTemplate(tempcard,el)).join('')
-        const output=tempOveriew.replace('{%PRODUCT_CARDS%}',cardsHtml)
-        res.end(output)
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempcard, el))
+      .join("");
+    const output = tempOveriew.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
 
     //Product page
-    }else if(pathname==='/product'){
-        res.writeHead(200,{'Content-type':'text/html'})
-        
-        const product=dataObj[query.id]
-        const output=replaceTemplate(tempProduct,product)
-        res.end(output)
-    }
+  } else if (pathname === "/product") {
+    console.log(query)
+    const emoji=query.id
+    console.log(emoji)
+    const id=emojiToId[emoji]
+    res.writeHead(200, { "Content-type": "text/html" });
 
-    //API
-    else if(pathname==='/api'){
-            res.writeHead(200,{'Content-type':'application/json'})
-            res.end(data)
+    const product = dataObj[id];
+    // console.log(product)
 
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
 
-        
-        
-
-        
-
-        
-
-
-
-    }
-    //Not found
-    else{
-        res.writeHead(404,{
-            'Content-type':'text/html',
-            'my-own-header':'hello-world'
-        })
-        res.end('<h1>page not found!</h1>')
-    }
-})
-server.listen(8121,'127.0.0.1',()=>{
-    console.log('Listening to requests on port 8000')
-})
-
+  //API
+  else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  }
+  //Not found
+  else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>page not found!</h1>");
+  }
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
 
